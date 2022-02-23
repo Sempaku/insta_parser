31186b8c8af6a592acbd66e0b0adc5701a4c46fd
Sempaku <cemen72421@gmail.com>
2022-02-23 07:25:25 +0300

    +auth

--- parser.py
+++ parser.py
@@ -1,5 +1,24 @@
 import instaloader
 
+
+target = input("Enter target username:")
 inst = instaloader.Instaloader()
-user = "sempaku_"
-inst.download_profile(user,profile_pic_only=True)
+#inst.download_profile(username,profile_pic=True,download_stories=True,)
+id = inst.check_profile_id(target)
+#inst.dirname_pattern()
+username = input("Login: ")
+
+#inst.two_factor_login(two_factor_code)
+
+inst.interactive_login(username)
+
+#inst.get_stories(id)
+#inst.download_stories()
+'''
+for story in inst.get_stories():
+    # story is a Story object
+    for item in story.get_items():
+        # item is a StoryItem object
+        inst.download_storyitem(item, ':stories')
+'''
+
