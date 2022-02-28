baf6d7b6d83ca57bc2563628b2f0029ab0a8639a
sempaku_win <cemen72421@gmail.com>
2022-02-28 17:43:52 +0300

    + biography + full name - save_metadata

--- parser.py
+++ parser.py
@@ -1,11 +1,19 @@
-# TODO: 1)target_followers --> add output in file +
-#       2)
+# TODO: 1) target_followers --> add output in file (+)
+#       2) igtv
+#       3) update
+#       4) highlights
+#       5) followees
+#       6) bio (+)
+#       7) check who followed in your account
+#       8) full name (+)
+#       9) create folder TARGETS and mv all targets in dir
+
 import colorama
 from colorama import Fore, Style
 from termcolor import cprint, colored
 import instaloader
 from time import sleep
-inst = instaloader.Instaloader()
+inst = instaloader.Instaloader(save_metadata=False)
 colorama.init()
 logo = """
   ___           _            ____
@@ -22,45 +30,54 @@ cprint(logo, 'cyan')
 print(Fore.GREEN + Style.BRIGHT)
 target = input("Enter target username:")
 
-
+# Profile
 id = inst.check_profile_id(target)
 profile = instaloader.Profile.from_username(inst.context, target)
+full_name_target = profile.full_name
+bio_target = profile.biography
+
 
 # Check block
 private_check = profile.is_private
 verify_check = profile.is_verified
 business_check = profile.is_business_account
+
 verify = "VERIFIED" if verify_check is True else "UNVERIFIED"
 privacy = "PRIVATE" if private_check is True else "UNPRIVATE"
 business = "BUSINESS" if business_check is True else "FREE"
 
-print(f"""{colored(target, 'red')} have {colored(privacy, 'red')}
-      {colored(verify, 'red')}, {colored(business, 'red')} account.""")
+print(f"""\n{colored(target, 'red')} --> {colored(full_name_target,'red')} have
+      {colored(privacy, 'red')},
+      {colored(verify, 'red')},
+      {colored(business, 'red')} account.""")
 if business_check is True:
     business_name = profile.business_category_name
     print(f"Business name: {colored(business_name,'red')}")
+if bio_target != "":
+    print(f"Profile biography:\n{colored(bio_target,'red')}")
 print(Fore.GREEN + Style.BRIGHT)
 
 
-# Auth in Instagram
-def auth():
-    username = input("Login: ")
-    inst.interactive_login(username)
+def auth():  # Auth in Instagram
+    username = input(Fore.LIGHTGREEN_EX + "Login: ")
+    inst.interactive_login(username + Fore.GREEN)
 
-def down_story():
+
+def down_story():  # Download stories (auth)
     inst.get_stories(id)
     inst.download_stories()
     for story in inst.get_stories():  # story is a Story object
         for item in story.get_items():  # item is a StoryItem object
             inst.download_storyitem(item, ':stories')
-def down_avatar(target):  # Download Profile_picture (no auth)
+
+
+def down_avatar(target):  # Download profile picture (no auth)
     inst.download_profile(target, profile_pic_only=True,)
 
 
-def down_posts(target):
+def down_posts(target):  # Download posts (no auth)
     for post in profile.get_posts():
-        instaloader.Instaloader(
-            save_metadata=False).download_post(post, target=target)
+        inst.download_post(post, target=target)
 
 
 def target_followers():  # View target followers (auth)
@@ -75,8 +92,7 @@ def target_followers():  # View target followers (auth)
 
 def general_pick():
     try:
-        print(Fore.GREEN)
-        print("""
+        print(Fore.GREEN + """
 Menu:
 1)With authorization in Instagram
 2)Without authorization on Instagram
@@ -102,8 +118,10 @@ Menu:
 
 
 def auth_pick():
-    pass
-    print("auth_pick")
+    try:
+        auth()
+    except:
+        pass
 
 
 def noauth_pick():
@@ -112,12 +130,3 @@ def noauth_pick():
 
 '''general_pick()'''
 # inst.dirname_pattern()
-
-
-
-
-# down_avatar(target)
-# down_posts(target)
-# target_followers()
-# down_posts(target)
-# down_posts(target)
