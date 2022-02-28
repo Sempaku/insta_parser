ef86db9193e48a0a45fefb2514216b9df6b8dabd
sempaku_win <cemen72421@gmail.com>
2022-02-28 14:27:33 +0300

    + download story + add menu + add function auth()

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 # TODO: 1)target_followers --> add output in file +
 #       2)
 import colorama
-from colorama import Fore, Style, Back
+from colorama import Fore, Style
 from termcolor import cprint, colored
 import instaloader
+from time import sleep
 inst = instaloader.Instaloader()
 colorama.init()
 logo = """
@@ -12,12 +13,13 @@ logo = """
   | || '_ \/ __| __/ _` |   | |_) / _` | '__/ __|/ _ \ '__|
   | || | | \__ \ || (_| |   |  __/ (_| | |  \__ \  __/ |
  |___|_| |_|___/\__\__,_|   |_|   \__,_|_|  |___/\___|_|
-"""
+"""  # noqa: W605
 
 cprint(logo, 'cyan')
 
+
 # Input targer nickname
-print(Fore.BLUE + Style.BRIGHT)
+print(Fore.GREEN + Style.BRIGHT)
 target = input("Enter target username:")
 
 
@@ -32,19 +34,25 @@ verify = "VERIFIED" if verify_check is True else "UNVERIFIED"
 privacy = "PRIVATE" if private_check is True else "UNPRIVATE"
 business = "BUSINESS" if business_check is True else "FREE"
 
-print(f"{colored(target,'red')} have {colored(privacy,'red')},{colored(verify,'red')},{colored(business,'red')} account.")
+print(f"""{colored(target, 'red')} have {colored(privacy, 'red')}
+      {colored(verify, 'red')}, {colored(business, 'red')} account.""")
 if business_check is True:
     business_name = profile.business_category_name
     print(f"Business name: {colored(business_name,'red')}")
-print(Fore.BLUE + Style.BRIGHT)
+print(Fore.GREEN + Style.BRIGHT)
 
 
 # Auth in Instagram
+def auth():
+    username = input("Login: ")
+    inst.interactive_login(username)
 
-username = input("Login: ")
-inst.interactive_login(username)
-
-
+def down_story():
+    inst.get_stories(id)
+    inst.download_stories()
+    for story in inst.get_stories():  # story is a Story object
+        for item in story.get_items():  # item is a StoryItem object
+            inst.download_storyitem(item, ':stories')
 def down_avatar(target):  # Download Profile_picture (no auth)
     inst.download_profile(target, profile_pic_only=True,)
 
@@ -59,31 +67,57 @@ def target_followers():  # View target followers (auth)
     f = open(f'{target}_followers.txt', 'w+')
     followers = profile.get_followers()
     for follower in followers:
-<<<<<<< HEAD
-        print(follower)
-
-
-
-=======
         fol = str(follower)
         print(Fore.LIGHTGREEN_EX + fol[8:-1])
         f.write('\n' + fol[8:-1] + '\n')
     f.close()
->>>>>>> be1a8cb18a3421da79c091b27946a4c00ee53cdc
 
 
+def general_pick():
+    try:
+        print(Fore.GREEN)
+        print("""
+Menu:
+1)With authorization in Instagram
+2)Without authorization on Instagram
+0)Exit
+              """)
+
+        general_choise = input("Input number:").lower()
+
+        if general_choise == "1":
+            auth_pick()
+        elif general_choise == "2":
+            noauth_pick()
+        elif general_choise == "0" or "exit" or "quit":
+            exit("Bye!")
+        else:
+            print(Fore.RED + "Wrong input!"+Fore.GREEN)
+            sleep(1)
+            general_choise()
+    except(ValueError, TypeError):
+        print(Fore.RED + "Error!"+Fore.GREEN)
+        sleep(1)
+        general_pick()
+
+
+def auth_pick():
+    pass
+    print("auth_pick")
+
+
+def noauth_pick():
+    print("noauth_pick")
+
+
+'''general_pick()'''
 # inst.dirname_pattern()
 
 
-# inst.get_stories(id)
-# inst.download_stories()
-# for story in inst.get_stories():
-#    # story is a Story object
-#    for item in story.get_items():
-#        # item is a StoryItem object
-#        inst.download_storyitem(item, ':stories')
-#
+
+
 # down_avatar(target)
 # down_posts(target)
-target_followers()
+# target_followers()
+# down_posts(target)
 # down_posts(target)
